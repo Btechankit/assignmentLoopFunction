d73d6571028c10d329ed68e57377c6164a49244c
Ankit Tiwari <ankittiwari2006vns@gmail.com>
2026-08-31 06:42:58 +0530

    Array new assignment

---
+++ Array.js
@@ -0,0 +1,110 @@
+// 1---------------
+// let fruits=["Apple","Mango","Orange","grapes","Lemon"];
+// console.log(fruits)
+
+// 2-----------------
+// let webDevlopment=["HTML","CSS"];
+// webDevlopment.push("JavaScript");
+// console.log(webDevlopment)
+
+// 3-------------------------
+// let webDevlopment=["HTML","CSS","JavaScript"];
+// webDevlopment.pop();
+// console.log(webDevlopment)
+
+// 4-------------------
+// let webDevlopment=["Red","Blue","Green"];
+// webDevlopment.shift();
+// console.log(webDevlopment)
+
+// 5------------------
+// let webDevlopment=["CSS","JavaScript"];
+// webDevlopment.unshift("HTML");
+// console.log(webDevlopment)
+
+// 6----------------------------
+// let webApp=["HTML","CSS"];
+// webApp.push("JavaScript")
+// webApp.push("React")
+// console.log(webApp)
+
+// 7--------------------------
+// let webApp=["HTML","CSS","React","JavaScript"];
+// webApp.splice(1, 1 );
+// console.log(webApp)
+
+// 8-----------------------------------
+// let arr = ["HTML", "JavaScript"];
+// arr.splice(1, 0, "CSS");
+// console.log(arr);                 
+// -----------------------------------
+ 
+// 9-----------------------------------
+// let webApp=["HTML","CSS","Java"];
+// webApp[2]= "JavaScript";
+// console.log(webApp)
+
+// 10--------------------;
+// let arr = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+// let result = arr.slice(1, 4);
+// console.log(result);
+
+// 11---------------------------
+// let arr = ["HTML", "CSS", "JavaScript", "React"];
+// let copy = arr.slice();
+// console.log(copy);
+
+// 12------------------------------------
+// var indexArray=["HTML", "CSS", "JavaScript", "React", "Node.js"];
+// console.log(indexArray.indexOf("JavaScript"));
+
+// 13------------------------------
+// var indexArray=["HTML", "CSS", "JavaScript", "React", "Node.js"];
+// console.log(indexArray.indexOf("React"));
+
+// 14------------------------------------
+// let detail = [
+//     { name: "ram", age: 20 },
+//     { name: "rahul", age: 22 },
+//     { name: "priya", age: 21 }
+// ];
+
+// console.log(detail.find((name) => {
+//     return name.name === "ram";
+// }));
+
+// 15-----------------------------------
+// let detail = [
+//     { name: "Rahul", age: 20 },
+//     { name: "Priya", age: 22 }
+// ];
+
+// let result = detail.findIndex((u) => {
+//     return u.name === "Priya";
+// });
+
+// console.log(result);
+
+// 16----------------------------------------
+// let original=[1,2,3,[4]];
+// console.log(original.flat(2))
+
+// 17-------------------------------------------
+// let original=[1, [2, [3, 4]]];
+// console.log(original.flat(2))
+
+// 18---------------------------------------------
+// let colors = ["Red", "Blue", "Green", "Yellow", "Black"];
+// colors.forEach((color) => {
+//     console.log(color);
+// }); 
+
+// 19-----------------------------------------------
+// var indexArray=["HTML", "CSS", "JavaScript", "React", "Node.js"];
+// indexArray.forEach((a) => {console.log(a)});
+
+// 20-------------------------------------------------------
+// var indexArray=["HTML", "CSS", "JavaScript"];
+// indexArray.push("React");
+// indexArray.shift();
+// console.log(indexArray)
